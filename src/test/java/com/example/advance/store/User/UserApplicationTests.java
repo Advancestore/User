36958c8a8d4f16115dee5272c610ea489705a34d
Pradeep <advancestores01@gmail.com>
2023-09-21 01:54:13 +0530

    Initial Commit

---
+++ src/test/java/com/example/advance/store/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advance.store.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
